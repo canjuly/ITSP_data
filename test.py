4c2ed38b02e3718d6cd524b15f1edbbbb29f1864
Your Name <you@example.com>
2020-11-19 14:07:01 +0800

    first commit

---
+++ test.py
@@ -0,0 +1,91 @@
+import os
+import shutil
+
+def fun1():
+    cwd = os.getcwd()
+    lab_list = os.listdir(cwd)
+    data_path = os.path.join(cwd, 'data')
+    os.mkdir(data_path)
+
+    for lab in lab_list:
+        lab_path = os.path.join(cwd, lab)
+        # print(lab_path)
+        if lab.find('Lab') != -1:
+            problem_list = os.listdir(lab_path)
+            for problem_id in problem_list:
+                print(problem_id)
+                data_problem_path = os.path.join(data_path, problem_id)
+                os.mkdir(data_problem_path)
+                data_problem_AC_path = os.path.join(data_problem_path, 'AC_c')
+                os.mkdir(data_problem_AC_path)
+                data_problem_WA_path = os.path.join(data_problem_path, 'WA_c')
+                os.mkdir(data_problem_WA_path)
+
+                problem_path = os.path.join(lab_path, problem_id)
+                solution_list = os.listdir(problem_path)
+                for solution_id in solution_list:
+                    solution_path = os.path.join(problem_path, solution_id)
+                    if solution_id.find('buggy') != -1:
+                        data_solution_path = os.path.join(data_problem_WA_path, solution_id)
+                    elif solution_id.find('correct') != -1:
+                        data_solution_path = os.path.join(data_problem_AC_path, solution_id)
+                    shutil.copy(solution_path, data_solution_path)
+
+        elif lab.find('py') == -1:
+            print('-------------------------------------')
+            file_list = os.listdir(lab_path)
+            for i in file_list:
+                file_path = os.path.join(lab_path, i)
+                problem_id = i.split('.')[1]
+                # file_type = i.split('.')[0]
+                data_problem_path = os.path.join(data_path, problem_id)
+                data_test_path = os.path.join(data_problem_path, 'TEST_DATA_TCG1')
+                if not os.path.exists(data_test_path):
+                    print(problem_id)
+                    os.mkdir(data_test_path)
+                data_file_path = os.path.join(data_test_path, i)
+                shutil.copy(file_path, data_file_path)
+
+
+def fun2():
+    cwd = os.getcwd()
+    problem_list = os.listdir(cwd)
+    for problem in problem_list:
+        if problem.find('py') != -1:
+            continue
+        print(problem)
+        problem_path = os.path.join(cwd, problem)
+        tag_path = os.path.join(problem_path, 'Tag_c')
+        os.mkdir(tag_path)
+        ac_path = os.path.join(problem_path, 'AC_c')
+        file_list = os.listdir(ac_path)
+        cnt = 1
+        for file_name in file_list:
+            ac_file = file_name
+            wa_file = file_name.replace('correct', 'buggy')
+            tag_file = os.path.join(tag_path, str(cnt) + '.txt')
+            with open(tag_file, 'w+') as f:
+                f.writelines(wa_file + '\n')
+                f.writelines(ac_file + '\n')
+            cnt += 1
+        # break
+
+def fun3():
+    cwd = os.getcwd()
+    problem_list = os.listdir(cwd)
+    for problem in problem_list:
+        if problem.find('py') != -1:
+            continue
+        print(problem)
+        problem_path = os.path.join(cwd, problem)
+        test_path = os.path.join(problem_path, 'TEST_DATA_TCG1')
+        test_list = os.listdir(test_path)
+        for i in test_list:
+            case_path = os.path.join(test_path, i)
+            new_case_path = case_path.replace('.txt', '')
+            os.rename(case_path, new_case_path)
+
+
+
+if __name__ == "__main__":
+    fun3()
